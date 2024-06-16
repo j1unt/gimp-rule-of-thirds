e717b17b96743844fe311e8ae7ccf2e48db09cae
John Lunt <johnlunt508@gmail.com>
2024-06-16 18:59:47 -0400

    Added script

---
+++ example.py
@@ -0,0 +1,47 @@
+from gimpfu import *
+
+def apply(image, tdrawable, brush_radius=2, make_centre=True):
+
+    h = image.height
+    w = image.width
+
+    hpoints1 = [0,h/3 , w,h/3]
+    hpoints2 = [0,h/2 , w,h/2]
+    hpoints3 = [0,2*(h/3) , w,2*(h/3)]
+    vpoints1 = [w/3,0 , w/3,h]
+    vpoints2 = [w/2,0 , w/2,h]
+    vpoints3 = [2*(w/3),0 , 2*(w/3),h]
+
+    brush = pdb.gimp_brush_new("Block")
+    pdb.gimp_brush_set_hardness(brush, 1.0)
+    pdb.gimp_brush_set_shape(brush, BRUSH_GENERATED_SQUARE)
+    pdb.gimp_brush_set_radius(brush, brush_radius)
+    pdb.gimp_context_set_brush(brush)
+
+    pdb.gimp_paintbrush(image.layers[0], 0, len(hpoints1), hpoints1, 0, 0)
+    if make_centre:
+        pdb.gimp_paintbrush(image.layers[0], 0, len(hpoints2), hpoints2, 0, 0)
+    pdb.gimp_paintbrush(image.layers[0], 0, len(hpoints3), hpoints3, 0, 0)
+    pdb.gimp_paintbrush(image.layers[0], 0, len(vpoints1), vpoints1, 0, 0)
+    if make_centre:
+        pdb.gimp_paintbrush(image.layers[0], 0, len(vpoints2), vpoints2, 0, 0)
+    pdb.gimp_paintbrush(image.layers[0], 0, len(vpoints3), vpoints3, 0, 0)
+    
+
+register(
+    "Rule-of-Thirds",
+    "Applies the rule of thirds to an image",
+    "Applies the rule of thirds to an image",
+    "John Lunt",
+    "MIT Licence",
+    "2024",
+    "<Image>/Filters/RuleOfThirds",
+    "",
+    [
+        (PF_INT, "brush_radius", "Brush radius", 2),
+        (PF_TOGGLE, "make_centre", "Draw centered guides", True),
+    ],
+    [],
+    apply)
+
+main()
